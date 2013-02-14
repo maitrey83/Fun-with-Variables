c95502a5583f9eecc4ccd4451a8d7e372bb033ef
S <s>
2013-02-14 13:21:42 -0500

    {"type":"TI", "runs":"1"}

--- CookieDivvying.java
+++ CookieDivvying.java
@@ -9,13 +9,20 @@ public class CookieDivvying extends ConsoleProgram {
 		/* Everyone should see cookies! */
 		setFont("DejaVuSerif-BOLD-24");
 		
-		int numCookies = readInt("How many cookies? ");
+		/*int numCookies = readInt("How many cookies? ");
 		int numKids    = readInt("How many kids?");
 		
 		int cookiesPerKid = numCookies / numKids;
 		int cookiesForYou = numCookies % numKids;
 		
 		println("Cookies per kid: " + cookiesPerKid);
-		println("Cookies for you: " + cookiesForYou);		
+		println("Cookies for you: " + cookiesForYou);	*/
+		
+		
+		println("This program converts fahrenheit to celsius");
+		//int fahrenheit = 45;
+		int fahrenheit = readInt("Enter Fahrenheit = ");
+		int celsius = (fahrenheit - 32) * (5 / 9);
+		println("Celsius is - " + celsius + " degree");
 	}
 }
