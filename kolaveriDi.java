c0b6ffe8180043377d079ae4fff9b86b7d0308df
S <s>
2013-02-14 20:38:25 -0500

    {"type":"TI", "runs":"0"}

--- kolaveriDi.java
+++ kolaveriDi.java
@@ -1,4 +1,18 @@
+import acm.program.*;
 
-public class kolaveriDi {
+
+public class kolaveriDi extends ConsoleProgram {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		
+	public void run() {
+		setFont("DejaVuSerif-BOLD-24");
+	
+		println("WHy this Why this Kolaveri Kolaveri Di");	
+		
+	}
 
 }
