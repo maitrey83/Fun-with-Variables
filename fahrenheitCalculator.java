2b2cef39ee2092675001cfdf6151a965aca472a1
S <s>
2013-02-16 11:36:54 -0500

    {"type":"TI", "runs":"1"}

--- fahrenheitCalculator.java
+++ fahrenheitCalculator.java
@@ -6,7 +6,7 @@ public class fahrenheitCalculator extends ConsoleProgram {
 	public void run () {
 		int fahrenheit = readInt ("Enter Fahrenheit =");
 		int celsius = fahrenheit - 32;
-		int celsius1 = (int) (celsius1 * 0.56);
+		int celsius1 = (int) (celsius * 0.56);
 		println("Celsius is:"+ celsius1);
 	}
 	
