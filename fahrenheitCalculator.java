32815a1e1ffac48c80e8ac2aa8870730c9cbc2d8
S <s>
2013-02-20 19:21:48 -0500

    {"type":"TI", "runs":"1"}

--- fahrenheitCalculator.java
+++ fahrenheitCalculator.java
@@ -4,15 +4,7 @@ import acm.program.*;
 public class fahrenheitCalculator extends ConsoleProgram {
 
 	public void run () {
-		int fahrenheit = 0;
-		if (celsius(fahrenheit) > 10) {
-			println("Yuppie this is summer");
-		} else if (celsius(fahrenheit) < 0) {
-		println("Ahhh still winter hasn't gone");
-	} else {
-		println("dont know yet");
-	}
-	}
+		
 		private int celsius(int fahrenheit) {
 			int celsius = 0; 
 			celsius = (fahrenheit - 32)*(5/9);
