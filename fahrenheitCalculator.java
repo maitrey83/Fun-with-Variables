50b2f271b39e1f0ec1c9047b5f73459730bd8942
S <s>
2013-02-21 16:44:57 -0500

    {"type":"TI", "runs":"4"}

--- fahrenheitCalculator.java
+++ fahrenheitCalculator.java
@@ -1,12 +1,15 @@
 import acm.program.*;
 
-
+//Fahrenheit Method
 public class fahrenheitCalculator extends ConsoleProgram {
-
+	private static final int value2 = 32;
+	private static final double value1 = 0.55;
 	public  void run () {
+		//user enters fahrenheit temperature
 		int fahrenheit = readInt("Fahrenheit: ");
-		
+		//The copy of variable fahrenheit is being transferred to method called Cels(fahrenheit)
 		println("Celsius: " + cels(fahrenheit));
+		//using if control statement it determines the current temperature
 		if (cels(fahrenheit) > 0){
 			println("Yuppie Summer is back!!!");
 		} if (cels(fahrenheit) == 0){
@@ -15,9 +18,9 @@ public class fahrenheitCalculator extends ConsoleProgram {
 			println("It can snow anytime");
 		}
 	}
-		private int cels(int fahrenheit) {
-			double value1 = 0.54;
-			double value2 = 32;
+	//created Method or function for converting fahrenheit to celsius
+	private int cels(int fahrenheit) {
+			//equation celsius = (fahrenheit - 32)*(5/9)
 			int celsius = (int) ((fahrenheit - value2)*value1);
 			return celsius;	
 		}	
