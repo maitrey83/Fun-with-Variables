1d17cf75773b4a37bbaa75c7bc9b065cfe05816e
S <s>
2013-02-20 19:41:51 -0500

    {"type":"TI", "runs":"1"}

--- fahrenheitCalculator.java
+++ fahrenheitCalculator.java
@@ -4,7 +4,7 @@ import acm.program.*;
 public class fahrenheitCalculator extends ConsoleProgram {
 
 	public  void run () {
-		println("Celsius" + celsius(32));
+		println("Celsius " + celsius(32));
 	}
 		private int celsius(int fahrenheit) {
 			int celsius = (fahrenheit - 32)*(5/9);
