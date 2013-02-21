7dd44f2b62bbbcff4430ef2dc71b7655688e3078
S <s>
2013-02-21 14:31:11 -0500

    {"type":"TI", "runs":"2"}

--- fahrenheitCalculator.java
+++ fahrenheitCalculator.java
@@ -7,14 +7,14 @@ public class fahrenheitCalculator extends ConsoleProgram {
 		int fahrenheit = readInt("Fahrenheit: ");
 		
 		println("Celsius: " + cels(fahrenheit));
-		if (cels(fahrenheit) > 10){
+		if (cels(fahrenheit) > 0){
 			println("Yuppie Summer is back!!!");
 		} if (cels(fahrenheit) <= 0){
 			println("it's freezing!!!");
 			
-		} else {
-			print("Gosh!!! I hate winter");
-		}
+		} //else {
+			//print("Gosh!!! I hate winter");
+		//}
 		
 	}
 		private int cels(int fahrenheit) {
